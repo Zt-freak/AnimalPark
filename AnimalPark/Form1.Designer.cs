95b06da9ddbd14034257497647512a4405a16a1e
Zt-freak <bnkho98@hotmail.com>
2021-01-19 18:36:34 +0100

    initial commit

---
+++ AnimalPark/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace AnimalPark
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.VisitorName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Birthday = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cost = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Add person:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(60, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Birthday:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(70, 69);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.VisitorName,
+            this.Birthday});
+            this.dataGridView1.Location = new System.Drawing.Point(19, 101);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(251, 276);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // VisitorName
+            // 
+            this.VisitorName.HeaderText = "Name";
+            this.VisitorName.Name = "Name";
+            this.VisitorName.ReadOnly = true;
+            // 
+            // Birthday
+            // 
+            this.Birthday.HeaderText = "Birthday";
+            this.Birthday.Name = "Birthday";
+            this.Birthday.ReadOnly = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 384);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "delete";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Cost});
+            this.dataGridView2.Location = new System.Drawing.Point(308, 13);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.Size = new System.Drawing.Size(261, 364);
+            this.dataGridView2.TabIndex = 7;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Cost
+            // 
+            this.Cost.HeaderText = "Cost";
+            this.Cost.Name = "Cost";
+            this.Cost.ReadOnly = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(195, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 426);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn VisitorName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Birthday;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cost;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ AnimalPark/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnimalPark
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        private List<Visitor> visitors = new List<Visitor>();
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.visitors.Add(new Visitor(this.textBox1.Text, this.dateTimePicker1.Value));
+            this.dataGridView1.Rows.Add(this.textBox1.Text, this.dateTimePicker1.Value);
+            this.updateSub();
+        }
+
+        private void updateSub()
+        {
+            this.dataGridView2.Rows.Clear();
+            this.dataGridView2.Rows.Add();
+
+            DateTime today = DateTime.Today;
+            int adultCount = 0;
+            int childCount = 0;
+            foreach (Visitor visitor in this.visitors)
+            {
+                int age = (new DateTime(1, 1, 1) - (today - visitor.birthday)).Year - 1;
+                Console.WriteLine(age);
+                if (age > 18)
+                {
+                    adultCount++;
+                }
+                else
+                {
+                    childCount++;
+                }
+            }
+            this.dataGridView2.Rows.Add("No. of adults", adultCount);
+            this.dataGridView2.Rows.Add("No. of children", childCount);
+            if (adultCount == 2 && childCount == 0)
+            {
+                this.dataGridView2.Rows.Add("cost", 61);
+            }
+            else if (adultCount == 2 && childCount > 0)
+            {
+                if (childCount == 1)
+                {
+                    this.dataGridView2.Rows.Add("cost", 71);
+                }
+                else if(childCount == 2)
+                {
+                    this.dataGridView2.Rows.Add("cost", 82);
+                }
+                else
+                {
+                    this.dataGridView2.Rows.Add("cost", 82+(childCount-2)*11);
+                }
+            }
+            else if(adultCount == 2 && childCount == 2)
+            {
+                this.dataGridView2.Rows.Add("cost", 82);
+            }
+            else if(adultCount == 1 && childCount == 0 && (new DateTime(1, 1, 1) - (today - visitors[0].birthday)).Year - 1 < 65)
+            {
+                this.dataGridView2.Rows.Add("cost", 30);
+            }
+            else if (adultCount == 1 && childCount == 0 && (new DateTime(1, 1, 1) - (today - visitors[0].birthday)).Year - 1 > 65)
+            {
+                this.dataGridView2.Rows.Add("cost", 26);
+            }
+            else if (adultCount == 2 && childCount == 0 && (new DateTime(1, 1, 1) - (today - visitors[0].birthday)).Year - 1 > 65 && (new DateTime(1, 1, 1) - (today - visitors[2].birthday)).Year - 1 > 65)
+            {
+                this.dataGridView2.Rows.Add("cost", 65);
+            }
+            else
+            {
+                this.dataGridView2.Rows.Add("cost", adultCount*30+childCount*11);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int index = this.dataGridView1.CurrentCell.RowIndex;
+
+            this.dataGridView1.Rows.RemoveAt(index);
+            this.visitors.RemoveAt(index);
+            this.updateSub();
+        }
+    }
+
+    public class Visitor
+    {
+        public string name;
+        public DateTime birthday;
+
+        public Visitor(string name, DateTime birthday) {
+            this.name = name;
+            this.birthday = birthday;
+        }
+    }
+}
